cf5ad6676b5a31cb4dfe308aa56add309b806894
callor <callor@callor.com>
2022-08-09 15:45:20 +0900

    todoList

--- src/main/java/com/callor/todo/controller/TodoController.java
+++ src/main/java/com/callor/todo/controller/TodoController.java
@@ -6,11 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.callor.todo.model.TodoVO;
 import com.callor.todo.service.TodoService;
-
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value="/todo")
@@ -19,6 +20,7 @@ public class TodoController {
 	@Autowired
 	private TodoService todoService;
 	
+	@ResponseBody
 	@RequestMapping(value={"/",""},method=RequestMethod.GET)
 	public String home(Principal principal,Model model) {
 
@@ -27,7 +29,6 @@ public class TodoController {
 		
 		model.addAttribute("TODOS", todoList);
 		return "todo/home";
-		
 	}
 	
 	
